bbffbfcc0ede3fa24410a5bfa56fa04a3857a2ee
gchwang3 <80876560+gchwang3@users.noreply.github.com>
2021-05-16 13:22:46 +0900

    Add files via upload

--- realcoin.py
+++ realcoin.py
@@ -1,6 +1,20 @@
 import pyupbit
 import time
 from collections import deque
+import datetime
+printlog_filename = 'coin_'+datetime.datetime.now().strftime('%Y_%m%d_%H_%M_%S')+'.log'
+
+coin_test = False #True
+
+def printlog(message, *args):
+    """인자로 받은 문자열을 파이썬 셸에 출력한다."""
+    print(datetime.datetime.now().strftime('[%m/%d %H:%M:%S]'), message, *args)
+    with open(printlog_filename, 'a') as out:
+        out.flush()                 
+        # print( message, *args, file=out)
+        print(datetime.datetime.now().strftime('[%m/%d %H:%M:%S]'), message, *args, file=out)                
+        out.flush()         
+
 
 class Realcoin(pyupbit.Upbit):
     def __init__(self, key0, key1):
@@ -12,14 +26,14 @@ class Realcoin(pyupbit.Upbit):
             if price != None:
                 return price
             else:
-                print("get_current_price wait")
+                printlog("get_current_price wait")
                 time.sleep(0.3)
 
     def buy_market_order(self, ticker, cash):
         while True:
             order = super().buy_market_order(ticker, cash)
             if order == None or 'error' in order:
-                print("buy_market_order wait", ticker, cash, order)
+                printlog("buy_market_order wait", ticker, cash, order)
                 time.sleep(0.3)
                 continue
             else:
@@ -31,7 +45,7 @@ class Realcoin(pyupbit.Upbit):
             if order != None and len(order['trades']) > 0:
                 return order
             else:
-                print("get_order_detail wait", uuid)
+                printlog("get_order_detail wait", uuid)
                 time.sleep(0.3)
 
     def get_outstanding_order(self, ticker):
@@ -40,7 +54,7 @@ class Realcoin(pyupbit.Upbit):
             if order != None and len(order) == 0:
                 return order
             else:
-                print("get_outstanding_order wait", ticker)
+                printlog("get_outstanding_order wait", ticker)
                 time.sleep(0.3)
 
     def get_balance(self, ticker="KRW"):
@@ -49,7 +63,7 @@ class Realcoin(pyupbit.Upbit):
             if volume != None:
                 return volume
             else:
-                print("get_balance wait", ticker)
+                printlog("get_balance wait", ticker)
                 time.sleep(0.3)
 
     def sell_limit_order(self, ticker, price, volume):
@@ -59,8 +73,7 @@ class Realcoin(pyupbit.Upbit):
             if order != None and "uuid" in order:
                 return order
             else:
-                print(price, volume, order)
-                print("sell_limit_order wait", ticker, price, volume, order)
+                printlog("sell_limit_order wait", ticker, price, volume, order)
                 time.sleep(0.3)
 
 class Real1Percent(Realcoin):
@@ -80,6 +93,9 @@ class Real1Percent(Realcoin):
         self.price_curr = None
         self.hold_flag = False
         self.wait_flag = False
+        self.remain_flag = False        
+        self.remain_price = 0                
+        self.remain_volume = 0                        
         self.cash = cash
         self.uuid = None
 #        self.cash = 10000
@@ -95,15 +111,24 @@ class Real1Percent(Realcoin):
         self.curr_ma120 = sum(self.ma120) / len(self.ma120)
 
         if self.hold_flag == False:
-            self.price_buy  = price_open * 1.01
-            self.price_sell = price_open * 1.02
-            # self.price_buy  = price_open
-            # self.price_sell = price_open
+            if coin_test:
+                # 손절가 테스트 
+                self.price_buy  = price_open * 0.99   
+                self.price_sell = price_open * 1.10
+            else:
+                self.price_buy  = price_open * 1.01
+                self.price_sell = price_open * 1.02
+
         self.wait_flag  = False
 
     def can_i_buy(self, price):
         # return self.wait_flag == False
-        return self.hold_flag == False and self.wait_flag == False and \
+        if coin_test:
+            # 손절가 테스트 
+            return self.hold_flag == False and self.wait_flag == False and \
+                price >= self.price_buy
+        else:
+            return self.hold_flag == False and self.wait_flag == False and \
             price >= self.price_buy and self.curr_ma15 >= self.curr_ma50 and \
             self.curr_ma15 <= self.curr_ma50 * 1.03 and self.curr_ma120 <= self.curr_ma50
 
@@ -116,18 +141,18 @@ class Real1Percent(Realcoin):
 
     def make_order(self):
         ret = self.buy_market_order(self.ticker, self.cash * 0.9995)
-        print("매수 주문")
+        order = ret                
+        printlog("매수 주문",order['created_at'][11:-6],order['market'],'매수/매도',order['side'], order['state'],'금액',order['price'], '잔량',order['remaining_volume'])                                        
 
-        order = self.get_order(ret['uuid'])
-        print(order['created_at'][0:-6],order['market'],order['side'], order['state'],order['price'], order['remaining_volume'])                                
-        print("매수 잔량", order['remaining_volume'])        
+        time.sleep(3)
+        #order = self.get_order(self.ticker)  # 호출시 error 발생하여, 현재 함수 탈출함. 
+        # printlog('order',order)
+        #printlog('주문정보',order['created_at'][0:-6],order['market'],order['side'], order['state'],order['price'], order['remaining_volume'])                                
         volume = self.get_balance(self.ticker)
-
         ret = self.sell_limit_order(self.ticker, self.price_sell, volume)
-        print("매도 주문", ret)
-        order = ret
-        print(order['created_at'][0:-6],order['market'],order['side'], order['state'],order['price'], order['remaining_volume'])                                        
-        self.uuid = ret['uuid']
+        order = ret                
+        printlog("매도 주문",order['created_at'][11:-6],order['market'],'매수/매도',order['side'], order['state'],order['price'], '잔량',order['remaining_volume'])                                        
+        # self.uuid = ret['uuid']
         self.hold_flag = True
 
     def make_sell_cancel_order(self):
@@ -136,15 +161,15 @@ class Real1Percent(Realcoin):
             if order['side'] == 'ask':            
                 ret = self.cancel_order(order['uuid'])
                 if ret == None or 'error' in ret:
-                    print("매도 취소 에러",ret)
+                    printlog("매도 취소 에러",ret)
                     time.sleep(0.5)
                 else:
-                    print("매도 취소")
+                    printlog("매도 취소")
 
         orders = self.get_order(self.ticker)                            
         for order in orders:    
             if order['side'] == 'ask':            
-                print('cancel wait')
+                printlog('cancel wait')
                 time.sleep(0.5)                                
 
     def make_sell_market_order(self):
@@ -153,30 +178,35 @@ class Real1Percent(Realcoin):
             ret = self.sell_market_order(self.ticker, volume)
             time.sleep(0.5)                        
             if ret == None or 'error' in ret:
-                print("시장가 매도 주문 에러", ret)
+                printlog("시장가 매도 주문 에러", ret)
                 time.sleep(0.5)
             else:
-                print("시장가 매도주문")
                 order = ret
-                print(order['created_at'][0:-6],order['market'],order['side'], order['state'],order['price'], order['remaining_volume'])                
+                printlog("시장가 매도주문",order['created_at'][11:-6],order['market'],'매수/매도',order['side'], order['state'],'금액',order['price'], '잔량',order['remaining_volume'])                                                        
                 break
 
         orders = self.get_order(self.ticker)                            
         for order in orders:    
             if order['side'] == 'ask':            
-                print('market ask wait')
+                printlog('market ask wait')
                 time.sleep(0.5)          
+        remain_cash = self.get_balance()
+        printlog("잔고 : ", remain_cash)
         self.hold_flag = False
         self.wait_flag = True
+        self.remain_flag = False            
+        self.remain_price = 0
+        self.remain_volume = 0
+
 
 
     def take_order(self):
         # ticker 가 입력될 경우, 미체결시 주문이 있을 경우 리턴함. 
         uncomp = self.get_order(self.ticker) 
-        print(uncomp)
-        print("매도완료")        
+        printlog(uncomp)
+        printlog("매도완료")        
         remain_cash = self.get_balance()
-        print("잔고 : ", remain_cash)
+        printlog("잔고 : ", remain_cash)
         self.hold_flag = False
         self.wait_flag = True
     # 매도 주문 확인
@@ -185,20 +215,26 @@ class Real1Percent(Realcoin):
         orders = self.get_order(self.ticker)
         ask_count = 0
         for order in orders:    
-            print(order['created_at'][0:-6],order['market'],order['side'], order['state'],order['price'], order['remaining_volume'])
+            # printlog('주문확인', order['created_at'][11:-6],order['market'],order['side'], order['state'],order['price'], order['remaining_volume'])                            
             if order['side'] == 'ask':
+                self.remain_price = order['price']
+                self.remain_volume = order['remaining_volume']
                 ask_count += 1
 
-        print("미채결 매도",ask_count)    
+#        printlog("미채결 매도",ask_count)    
         if ask_count == 0:
             self.hold_flag = False
             self.wait_flag = True
-            print("매도완료")                    
+            self.remain_flag = False            
+            self.remain_price = 0
+            self.remain_volume = 0
+            printlog("매도완료")                    
             remain_cash = self.get_balance()
-            print("잔고 : ", remain_cash)        
+            printlog("잔고 : ", remain_cash)        
         else:
             self.hold_flag = True
             self.wait_flag = True
+            self.remain_flag = True
         return ask_count == 0       
 
 if __name__ == "__main__":
@@ -210,16 +246,16 @@ if __name__ == "__main__":
     # price = upbit.get_current_price("KRW-BTC")
     # order = upbit.get_order_detail("34cabb23-8171-4fd1-8f05-f25e312461c7")
     #order = upbit.get_outstanding_order("KRW-BTC")
-    #print(order)
+    #printlog(order)
     coin = Real1Percent(key0, key1, "KRW-ADA", 100000)      
     #coin.make_sell_market_order()  # 시장가 판매 
     
     if coin.take_order_ask():
-        print("매도 성공")
+        printlog("매도 성공")
     else:
-        print("매도 실패, 주문 취소진행")
+        printlog("매도 실패, 주문 취소진행")
         coin.make_sell_cancel_order()    
         coin.make_sell_market_order()      
-        print("#33")
+        printlog("#33")
         coin.take_order_ask() 
 
--- realtime.py
+++ realtime.py
@@ -7,9 +7,22 @@ from collections import deque
 import realcoin
 
 import logging
- 
+
+printlog_filename = 'real_'+datetime.datetime.now().strftime('%Y_%m%d_%H_%M_%S')+'.log'
+
+def printlog(message, *args):
+    """인자로 받은 문자열을 파이썬 셸에 출력한다."""
+    print(datetime.datetime.now().strftime('[%m/%d %H:%M:%S]'), message, *args)
+    with open(printlog_filename, 'a') as out:
+        out.flush()                 
+        # print( message, *args, file=out)
+        print(datetime.datetime.now().strftime('[%m/%d %H:%M:%S]'), message, *args, file=out)                
+        out.flush()         
+
 tickers = ["KRW-ADA", "KRW-LTC", "KRW-DOGE", "KRW-ETH", "KRW-DOT", "KRW-HUNT",\
-            "KRW-CHZ", "KRW-SAND", "KRW-ATOM", "KRW-PLA"]
+            "KRW-CHZ", "KRW-SAND", "KRW-ATOM", "KRW-PLA", "KRW-XLM", "KRW-HBAR"]
+# tickers = ["KRW-DOGE"]
+
 
 class Consumer(threading.Thread):
     def __init__(self, q):
@@ -25,27 +38,29 @@ class Consumer(threading.Thread):
         self.ubit = upbit   
         self.pyupbit = pyupbit
         totalcash  = upbit.get_balance()
-        print("보유현금", totalcash)
-        cash = (totalcash * 0.2) / len(tickers)
-        print("보유현금", totalcash)
+        printlog("보유현금", totalcash)
+        cash = (totalcash * 0.95) / len(tickers)
+        # cash = 5500
 
         price_curr = pyupbit.get_current_price(tickers)        
-        print(price_curr)
+        printlog(price_curr)
+        printlog('tickers len:', len(tickers))
         for ticker in tickers:
-            print("ticker", ticker, cash )
+            printlog("ticker", ticker, cash )
 
 
         self.u = { }
         for ticker in tickers:
             self.u[ticker] = realcoin.Real1Percent(key0, key1, ticker, cash)
-        print("init end")
+        printlog("init end")
     def run(self):
         price_curr = { }
         for ticker in tickers:
             price_curr[ticker] = None
         i = 0
-
+        # check_buy = False        
         while True:
+
             try:
                 if not self.q.empty():
                     price_open = self.q.get()
@@ -53,35 +68,42 @@ class Consumer(threading.Thread):
                         self.u[ticker].update(price_open[ticker], price_curr[ticker])
 
                 price_curr = pyupbit.get_current_price(tickers)
-                time.sleep(0.03)                
-                # print(price_curr)
+                # time.sleep(0.03)                
+                #time.sleep(1)                                
+                time.sleep(0.5)                                                
+                # printlog(price_curr)
 
-                # print("##2")                
+                # printlog("##2")                
                 for ticker in tickers:
-                    if self.u[ticker].can_i_buy(price_curr[ticker]) :
-                        print("")
+                    # if self.u[ticker].can_i_buy(price_curr[ticker]) and check_buy == False:
+                    if self.u[ticker].can_i_buy(price_curr[ticker]):                        
+                        # check_buy = True                        
                         self.u[ticker].make_order()
+
 
                     if self.u[ticker].can_i_sell():
                         self.u[ticker].take_order_ask()                        
                     
                     if self.u[ticker].can_i_sell():                    
                         if self.u[ticker].can_i_sell_by_market(price_curr[ticker]):                        
-                            print("ticker", ticker, "손절")
-                            self.u[ticker].coin.make_sell_cancel_order()    
+                            printlog("ticker", ticker, "손절")
+                            self.u[ticker].make_sell_cancel_order()    
                             self.u[ticker].make_sell_market_order()                               
 
                 # 1 minutes
                 if i == (5 * 60 * 1):
+                # if i == (1):                
+                # if True :                    
                     now = datetime.datetime.now()
                     for ticker in tickers:
-                        print(f"[{now}] {ticker} : 현재가 {price_curr[ticker]}, 목표가 {self.u[ticker].price_buy}, ma {self.u[ticker].curr_ma15:.2f}/{self.u[ticker].curr_ma50:.2f}/{self.u[ticker].curr_ma120:.2f}, hold_flag {self.u[ticker].hold_flag}, wait_flag {self.u[ticker].wait_flag}")
+                        printlog(f"{ticker} : 현재가 {price_curr[ticker]}, 목표가 {self.u[ticker].price_buy}, 매도가 {self.u[ticker].remain_price}, 매도잔량 {self.u[ticker].remain_volume}, ma {self.u[ticker].curr_ma15:.2f}/{self.u[ticker].curr_ma50:.2f}/{self.u[ticker].curr_ma120:.2f}, h {self.u[ticker].hold_flag}, w {self.u[ticker].wait_flag}")
+
+                        
                     i = 0
                 i += 1
-                # print("##")
             except Exception as x:
-                # print(x.__class__.__name__)
-                print("error")
+                # printlog(x.__class__.__name__)
+                printlog("error")
                 time.sleep(0.2)
                 # break
 
